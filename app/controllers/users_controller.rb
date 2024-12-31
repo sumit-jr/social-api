25db44cce4f9c61b1ef22448bcb25c96633b00ea
sumit-jr <er.sahsumit@gmail.com>
2024-12-31 14:12:36 +0545

    feat: Add bcrypt gem, update User model, and enhance user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,18 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      render json: @user.as_json
+      render json: @user.as_json(only: [ :email, :first_name, :last_name ])
     else
       render json: { message: " Issue saving user ", error: @user.errors.full_messages }
     end
   end
 
+  def index
+    render json: { users: User.all.map(&:as_json) }
+  end
+
   private
   def user_params
-    params.require (:user).permit(:email, :password, :first_name, :last_name)
+    params.require(:user).permit(:email, :password, :first_name, :last_name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates_uniqueness_of :email
+
+  def as_json(opts = {})
+    super(opts.merge(only: [ :id, :email, :first_name, :last_name ]))
+  end
 end
