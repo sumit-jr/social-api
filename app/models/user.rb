7a31ce78d9e6d694a0450a5a8b0fe42fab4fdc6e
sumit-jr <er.sahsumit@gmail.com>
2024-12-31 14:54:42 +0545

    feat: Add username to User schema and validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username
 
   def as_json(opts = {})
-    super(opts.merge(only: [ :id, :email, :first_name, :last_name ]))
+    super(opts.merge(only: [ :id, :first_name, :last_name, :username ]))
   end
 end
